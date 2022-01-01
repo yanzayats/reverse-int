0da7d0d541e185ce33e7a32dbe129e63c74a83f4
YanZayats <vashotche@gmail.com>
2022-01-01 17:27:45 +0300

    complete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function reverse (n) {
-  
+    let a = n.toString().slice(1)
+    let b = n.toString()
+    let c = a.split('').reverse().join('')
+    let d = n.toString().slice(0,1)
+    let e = b.split('').reverse().join('')
+    if (d === '-') {
+        return Number(c)
+    } else {
+        return Number(e)
+    }
 }
